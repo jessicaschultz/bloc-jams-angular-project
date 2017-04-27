a557dada0440bf1f92533a3a7c27b7820cd16deb
Jessica Schultz <jessicaschultz1203@gmail.com>
2017-04-26 18:33:03 -0700

    checkpoint controllers completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,14 +9,16 @@
           $stateProvider
                 .state('landing', {
                       url: '/',
+                      controller: 'LandingCtrl as landing',
                       templateUrl: '/templates/landing.html'
                 })
-                .state('album',{
+                .state('album', {
                       url: '/album',
                       templateUrl: '/templates/album.html'
                 })
-                .state('collection',{
+                .state('collection', {
                       url: '/collection', 
+                      controller: 'CollectionCtrl as collection',
                       templateUrl: '/templates/collection.html'
                 });
     }
